a1d52dbbb5422eb4a1cf9dfdd3adb3fba4a0aa3f
Osiris Germosen <osirisgermosen@outlook.com>
2022-11-02 10:04:50 -0400

    Request GetBookById from gRPC Client

--- BookstoreClient/Program.cs
+++ BookstoreClient/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Bookstore;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7231");
+var client = new Inventory.InventoryClient(channel);
+
+#region Request GetBookById
+var bookId = new BookByIdRequest() { BookId = 1 };
+var book = await client.GetBookByIdAsync(bookId);
+
+Console.WriteLine($"Title: {book.Title}");
+Console.WriteLine($"Author: {book.Author}");
+Console.WriteLine($"ISBN: {book.Isbn}");
+Console.WriteLine($"Year: {book.Year}");
+Console.ReadKey();
+#endregion
