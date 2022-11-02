0f625538d40e1de1844f8be8d9f2f62d6eab0c55
Osiris Germosen <osirisgermosen@outlook.com>
2022-11-02 10:10:53 -0400

    Request GetBookList from gRPC Client

--- BookstoreClient/Program.cs
+++ BookstoreClient/Program.cs
@@ -6,12 +6,28 @@ var channel = GrpcChannel.ForAddress("https://localhost:7231");
 var client = new Inventory.InventoryClient(channel);
 
 #region Request GetBookById
-var bookId = new BookByIdRequest() { BookId = 1 };
-var book = await client.GetBookByIdAsync(bookId);
+//var bookId = new BookByIdRequest() { BookId = 1 };
+//var book = await client.GetBookByIdAsync(bookId);
 
-Console.WriteLine($"Title: {book.Title}");
-Console.WriteLine($"Author: {book.Author}");
-Console.WriteLine($"ISBN: {book.Isbn}");
-Console.WriteLine($"Year: {book.Year}");
+//Console.WriteLine($"Title: {book.Title}");
+//Console.WriteLine($"Author: {book.Author}");
+//Console.WriteLine($"ISBN: {book.Isbn}");
+//Console.WriteLine($"Year: {book.Year}");
+//Console.ReadKey();
+#endregion
+
+
+#region Request GetBookList
+var allBooks = new BookListRequest();
+var bookList = await client.GetBookListAsync(allBooks);
+
+foreach (var book in bookList.Books)
+{
+    Console.WriteLine($"Title: {book.Title}");
+    Console.WriteLine($"Author: {book.Author}");
+    Console.WriteLine($"ISBN: {book.Isbn}");
+    Console.WriteLine($"Year: {book.Year}");
+    Console.WriteLine(new String('*', 35));
+}
 Console.ReadKey();
 #endregion
